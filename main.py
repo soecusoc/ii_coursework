0fc18dae46880b3482f020a35a9276bddc41bfd3
soecusoc <timo.lintonen@gmail.com>
2016-04-26 15:49:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,40 @@ import socket
 import encryption
 import struct
 
-sTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = 'ii.virtues.fi'
-PORT = 10000
+def main():
 
-remote_ip = socket.gethostbyname(HOST)
+    sUDP_torecv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sUDP_torecv.bind(('', 10000))
+    
+    host = socket.gethostbyname('ii.virtues.fi')
+    print socket.gethostbyname(socket.gethostname())
+    
+    sTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sTCP.connect((host, 10000))
+    sTCP.sendall("HELO 10000\r\n")
+    
+    reply = sTCP.recv(1024)
+    port = int(reply.split()[1])
+    print reply
+    
+    sUDP_tosend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ini_msg = "Ekki-ekki-ekki-ekki-PTANG."
+    
+    data = struct.pack("!??HH64s", False, True, len(ini_msg), 0, ini_msg)
+    sUDP_tosend.sendto(ini_msg, ((socket.gethostname(), 10001)))
+    print data
+    
+    reply, addr = sUDP_torecv.recvfrom(1024)
+    print "%s from %s" % (reply, addr)
 
-print 'IP address of ' + HOST + ' is ' + remote_ip
+    sUDP_torecv.close()
+    sUDP_tosend.close()
+    sTCP.close()
 
-sTCP.connect((remote_ip, PORT))
 
-print 'Socket Connected to ' + HOST + ' on IP ' + remote_ip
-
-message = "HELO 10000\r\n"
-#for i in range(20):
-#    message = message + encryption.generateKey(64) + "\r\n"
-#message = message + ".\r\n"
-
-sTCP.sendall(message)
-
-print 'To server: ' + message
-
-reply = sTCP.recv(4096)
-
-print 'Server reply: ' + reply
-
-sTCP.close()
-
-PORT = int(reply.split()[1])
-
-print PORT
-
-sUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-sUDP.sendto('Ekki-ekki-ekki-ekki-PTANG.', (HOST, PORT))
-
-d = sUDP.recvfrom(1024)
-print d
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "Shutting down"
+        sys.exit(1)
